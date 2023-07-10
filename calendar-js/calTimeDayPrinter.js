bec80bd4279b4285afd68cccf5b6e3aba6d07c54
LDcourrielleur <93119484+LDcourrielleur@users.noreply.github.com>
2023-07-10 11:14:39 +0200

    Catchup minint to gmcd branch (step 2)

--- calendar-js/calTimeDayPrinter.js
+++ calendar-js/calTimeDayPrinter.js
@@ -372,6 +372,24 @@ calTimeDayPrinter.prototype = {
       div.appendChild(img);
     }
 
+    function hexToRgb(hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null;
+    }
+
+    let rgbaString = hexToRgb(calColor).r+","+hexToRgb(calColor).g+","+hexToRgb(calColor).b;
+
+    let style = "";
+    // annulé #6286: Les évts barrés ne sont pas barrés lors de l'impression
+    if (item.hasProperty("STATUS") && "CANCELLED"==item.getProperty("STATUS")) 
+    {
+        style="text-decoration-line: line-through;background-color:rgba("+rgbaString+",1);";
+    }
+
     // participants
     let parts=item.getAttendees({});
     if (parts && 0!=parts.length) {
@@ -406,6 +424,7 @@ calTimeDayPrinter.prototype = {
 
       let cl=div.getAttribute("class");
       div.setAttribute("class", cl+" itemEntier");
+	  div.setAttribute("style", style);
 
     } else{
 
@@ -422,7 +441,7 @@ calTimeDayPrinter.prototype = {
         end=this.getLigne(itemEndDate.hour, itemEndDate.minute);
       }
       
-      let style="grid-row-start:"+start+";grid-row-end:"+end+";";    
+      style+="grid-row-start:"+start+";grid-row-end:"+end+";";  
       div.setAttribute("style", style);
     }
 
